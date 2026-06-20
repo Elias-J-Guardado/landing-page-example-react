4343542dfc451b2a43d4fab94453e361fe980f5a
EliasGuardado <eliasjguardado2001@gmail.com>
2026-06-20 13:52:10 -0600

    Adding styles in navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,13 +6,26 @@ function Header() {
         >
             { /* Contenido del header */}
             <div className="absolute inset-0 bg-black/50" />
-            <nav className="relative z-10">
+            <nav className="relative z-10 ">
                 <a className="block text-white text-4xl font-black uppercase pt-4 p-4 cursor-pointer">CarGrow</a>
-                <div>
-                    <ul>
-                        <li className="text-white">¿Quines somos?</li>
-                        <li className="text-white">Servicios</li>
-                        <li className="text-white">Contacto</li>
+                <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10
+                justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading
+                focus:ring-2 focus:ring-neutral-ternary" aria-controls="navar-default" aria-expanded="false">
+                    <span className="sr-only">Abrir menu expandido</span>
+                    <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/></svg>
+                </button>
+                
+                <div className="hidden w-full md-block md:w-auto" id="navbar">
+                    <ul className="">
+                        <li className="text-white">
+                            <a href="#">¿Quines somos?</a>
+                        </li>
+                        <li className="text-white">
+                            <a href="#">Servicios</a>
+                        </li>
+                        <li className="text-white">
+                            <a href="#">Servicios</a>
+                        </li>
                     </ul>
                 </div>
             </nav>
@@ -22,9 +35,9 @@ function Header() {
                     Restauraciones Profesionales
                 </h1>
                 <p className="text-white/70 mt-2">Restauraciones al mejor precio</p>
-                <button className="mt-6 bg-[#151d36] text-white px-8 py-3 rounded-full uppercase font-bold w-fit">
+                <a className="mt-6 bg-[#151d36] text-white px-8 py-3 rounded-full uppercase font-bold w-fit cursor-pointer">
                     Agenda tu cita
-                </button>
+                </a>
             </div>
 
         </section>

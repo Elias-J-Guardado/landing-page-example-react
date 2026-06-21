9f7558417e0be4505acf1a52ca0d1858c4d5b3e6
EliasGuardado <eliasjguardado2001@gmail.com>
2026-06-21 12:05:54 -0600

    Adding styles to header and fixing bugs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,32 +6,32 @@ function Header() {
         >
             { /* Contenido del header */}
             <div className="absolute inset-0 bg-black/50" />
-            <nav className="relative z-10 ">
+            <nav className="relative z-10 flex items-center justify-between">
                 <a className="block text-white text-4xl font-black uppercase pt-4 p-4 cursor-pointer">CarGrow</a>
                 <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10
                 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading
                 focus:ring-2 focus:ring-neutral-ternary" aria-controls="navar-default" aria-expanded="false">
                     <span className="sr-only">Abrir menu expandido</span>
-                    <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/></svg>
+                    {/* <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/></svg> */}
                 </button>
                 
-                <div className="hidden w-full md-block md:w-auto" id="navbar">
-                    <ul className="">
-                        <li className="text-white">
-                            <a href="#">¿Quines somos?</a>
+                <div className="hidden w-full md:block md:w-auto px-4 mt-4" id="navbar">
+                    <ul className="flex gap-5">
+                        <li >
+                            <a href="#" className="text-white">¿Quines somos?</a>
                         </li>
-                        <li className="text-white">
-                            <a href="#">Servicios</a>
+                        <li >
+                            <a href="#" className="text-white">Servicios</a>
                         </li>
-                        <li className="text-white">
-                            <a href="#">Servicios</a>
+                        <li >
+                            <a href="#" className="text-white">Contacto</a>
                         </li>
                     </ul>
                 </div>
             </nav>
 
-            <div className="relative z-10 flex flex-col justify-end h-full p-12 py-100">
-                <h1 className="text-white text-6xl font-black uppercase">
+            <div className="relative z-10 flex flex-col justify-center h-full p-8 md:p-12">
+                <h1 className="text-white text-4xl md:text-6xl font-black uppercase">
                     Restauraciones Profesionales
                 </h1>
                 <p className="text-white/70 mt-2">Restauraciones al mejor precio</p>

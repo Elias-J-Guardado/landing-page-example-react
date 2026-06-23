a810d342717fb67985b5e5645a1637b1eb0adef7
EliasGuardado <eliasjguardado2001@gmail.com>
2026-06-23 15:34:19 -0600

    Add content to Services component

---
+++ src/components/Services.jsx
@@ -0,0 +1,15 @@
+function Services() {
+    return(
+        <div>
+            <p className="text-center text-4xl mb-8">Services</p>
+            <div className="">
+                <p className=""></p>
+                <div className="">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export {Services}

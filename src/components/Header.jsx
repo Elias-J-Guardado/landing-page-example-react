cb7355787dd02722f5b31b04a5f2eafdd7b30cb2
EliasGuardado <eliasjguardado2001@gmail.com>
2026-06-24 12:12:13 -0600

    Add animations in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,22 @@
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css'
+
 function Header() {
+    useEffect(() => {
+        AOS.init({duration: 800})
+    }, [])
+
     return (
         <section
             style={{ backgroundImage: 'url(/nelli-chaitanya-9yu5JZygjv0-unsplash.jpg)' }}
             className="relative w-full h-screen bg-cover bg-center bg-no-repeat"
         >
             { /* Contenido del header */}
-            <div className="absolute inset-0 bg-black/50" />
+            <div
+             className="absolute inset-0 bg-black/50" />
             <nav className="relative z-10 flex items-center justify-between">
-                <a className="block text-white text-6xl font-black uppercase pt-4 p-4 cursor-pointer">CarGrow</a>
+                <a data-aos="zoom-in" className="block text-white text-6xl font-black uppercase pt-4 p-4 cursor-pointer">CarGrow</a>
                 <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10
                 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading
                 focus:ring-2 focus:ring-neutral-ternary" aria-controls="navar-default" aria-expanded="false">
@@ -17,25 +26,25 @@ function Header() {
                 
                 <div className="hidden w-full md:block md:w-auto px-4 mt-4" id="navbar">
                     <ul className="flex gap-5">
-                        <li >
-                            <a href="#" className="text-white text-2xl">¿Quines somos?</a>
+                        <li>
+                            <a data-aos="zoom-in" href="#" className="text-white text-2xl">¿Quines somos?</a>
                         </li>
                         <li >
-                            <a href="#" className="text-white text-2xl">Servicios</a>
+                            <a data-aos="zoom-in" href="#" className="text-white text-2xl">Servicios</a>
                         </li>
                         <li >
-                            <a href="#" className="text-white text-2xl">Contacto</a>
+                            <a data-aos="zoom-in" href="#" className="text-white text-2xl">Contacto</a>
                         </li>
                     </ul>
                 </div>
             </nav>
 
             <div className="relative z-10 flex flex-col justify-center h-full p-8 md:p-12">
-                <h1 className="text-white text-5xl md:text-6xl font-black uppercase">
+                <h1 data-aos="zoom-in" className="text-white text-5xl md:text-6xl font-black uppercase">
                     Restauraciones Profesionales
                 </h1>
-                <p className="text-white/70 mt-2 text-2xl">Restauraciones al mejor precio</p>
-                <a className="mt-6 bg-[#151d36] text-white p-4 rounded-full uppercase font-bold w-fit cursor-pointer text-center 
+                <p data-aos="zoom-in" className="text-white/70 mt-2 text-2xl">Restauraciones al mejor precio</p>
+                <a data-aos="zoom-in" className="mt-6 bg-[#151d36] text-white p-4 rounded-full uppercase font-bold w-fit cursor-pointer text-center 
                 text-2xl hover:bg-[#1C2B47] focus:outline-offset-2 focus:outline-bg[#203052]">
                     Agenda tu cita
                 </a>

f548a403d05db6be653603486780f841197fd622
EliasGuardado <eliasjguardado2001@gmail.com>
2026-06-29 15:56:27 -0600

    Add classes in Service component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -11,14 +11,14 @@ function Services() {
     return(
         <section className="px-12 py-16">
             <p className="text-center text-4xl mb-8">Services</p>
-            <div className="flex gap-6 overflow-x-auto pb-4 no-scrollbar">
+            <div className=" flex flex-row gap-3 justify-center">
                 {services.map((service, i) => (
                     <div 
                         key={i}
-                        className="min-w-64 border border-white/20 bg-[#5e698c] rounded-2xl p-6 flex flex-col gap-2 shrink-0"
+                        className="w-64 border border-white/20 bg-[#5e698c] rounded-2xl p-6 flex flex-col gap-2 shrink-0 text-center"
                     >
-                        <h3> {service.title} </h3>
-                        <p> {service.descripcion} </p>
+                        <h3 className="text-white text-4xl"> {service.title} </h3>
+                        <p className="text-yellow-500 text-2xl"> {service.descripcion} </p>
                     </div>
                 ))}
             </div>

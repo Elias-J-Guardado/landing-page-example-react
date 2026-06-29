6e2203596a6a092db084b3c808ca1d386787ab6b
EliasGuardado <eliasjguardado2001@gmail.com>
2026-06-29 16:28:48 -0600

    Fix bugs and classes

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,21 +1,22 @@
 function Services() {
 
     const services = [
-        {title: "Restauración completa", descripcion: "Devolvemos tu auto a su estado original"},
-        {title: "Restauración completa", descripcion: "Devolvemos tu auto a su estado original"},
-        {title: "Restauración completa", descripcion: "Devolvemos tu auto a su estado original"},
-        {title: "Restauración completa", descripcion: "Devolvemos tu auto a su estado original"},
-        {title: "Restauración completa", descripcion: "Devolvemos tu auto a su estado original"},
+        {title: "Restauración completa", descripcion: "Devolvemos tu auto a su estado original, cada detalle cuidado al máximo."},
+        {title: "Pintura y Carrocería", descripcion: "Corrección de abolladuras y pintura personalizada con acabado profesional."},
+        {title: "Tapicería", descripcion: "Restauración de interiores, asientos y techo con materiales de calidad."},
+        {title: "Motor y Mećanica", descripcion: "Reconstrucción de motor y transmisión para máximo rendimiento."},
+        {title: "Llantas y Mécanica", descripcion: "Alineación, balanceo y suspensión deportiva a tu medida."},
+        {title: "Detailing", descripcion: "Pulido, encerado y protección de pintura para un acabado impecable."},
     ]
 
     return(
         <section className="px-12 py-16">
             <p className="text-center text-4xl mb-8">Services</p>
-            <div className=" flex flex-row gap-3 justify-center">
+            <div className=" grid grid-cols-1 md:grid-cols-3 gap-3">
                 {services.map((service, i) => (
                     <div 
                         key={i}
-                        className="w-64 border border-white/20 bg-[#5e698c] rounded-2xl p-6 flex flex-col gap-2 shrink-0 text-center"
+                        className="w-full md:min-w-64 md:shrink-0 border border-white/20  rounded-2xl p-6 flex flex-col gap-2 shrink-0 text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
                     >
                         <h3 className="text-white text-4xl"> {service.title} </h3>
                         <p className="text-yellow-500 text-2xl"> {service.descripcion} </p>

546599ade806154a24e0cd7ad2699e29a021028e
EliasGuardado <eliasjguardado2001@gmail.com>
2026-06-30 15:36:41 -0600

    Adding form to Form.jsx component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react"
+
 function Form() {
-    return(
-        console.log('From Form')
+    const [nombre, setNombre] = useState("");
+
+    function handleChange(e) {
+        setNombre(e.target.value);
+    }
+
+    return (
+        <section>
+            <div className="flex justify-center flex-col items-center">
+                <div>
+                    <h2>Contacto</h2>
+                </div>
+                <form action="">
+                    <label htmlFor="">Nombre</label>
+                    <input type="text" value={nombre} placeholder="Jhon Doe" onChange={handleChange} />
+                    <div>
+                        <label htmlFor="">Asunto</label>
+                        <input type="text" value={nombre} placeholder="Jhon Doe" onChange={handleChange} />
+                        <label htmlFor="">Telefono</label>
+                        <input type="text" value={nombre} placeholder="Jhon Doe" onChange={handleChange} />
+                    </div>
+                     <label htmlFor="">Correo</label>
+                    <input type="text" value={nombre} placeholder="Jhon Doe" onChange={handleChange} />
+                     <label htmlFor="">Mensaje</label>
+                    <textarea type="text" value={nombre} placeholder="Jhon Doe" onChange={handleChange} />
+                    <div>
+                        <input type="button" value="Enviar" />
+                    </div>
+                </form>
+            </div>
+        </section>
     )
 }
 
-export {Form}
+export { Form }

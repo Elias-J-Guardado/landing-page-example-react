899ae3cb845b93245268d4edec8139c269f6aed7
EliasGuardado <eliasjguardado2001@gmail.com>
2026-07-06 18:51:44 -0600

    Add emailjs API

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 function Footer() {
     return(
-        <div className="px-4 py-4 text-center text-2xl bg-linear-to-r from-red-500 via-red-800 to-black">
+        <section className="px-4 py-4 text-center text-2xl bg-linear-to-r from-red-500 via-red-800 to-black">
             <p className="text-white">&copy;GrowCar 2026 Todos los derechos reservados</p>
-        </div>
+        </section>
     )
 }
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,39 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import AOS from 'aos';
 import 'aos/dist/aos.css'
+import emailjs from "@emailjs/browser";
 
 function Form() {
+
+
+    //UseEffect AOS
     useEffect(() => {
-        AOS.init({duration: 500})
+        AOS.init({ duration: 500 })
     }, [])
 
+
+    //emailjs Api
+    const formulario = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm('', '', formulario.current, {
+                publicKey: '',
+            })
+            .then(
+            () => {
+                console.log('Conectado')
+
+            },
+                (error) => {
+                    console.log('Operación fallida...', error.text);
+                },
+            );
+    };
+
+    //Form set
     const [form, setForm] = useState({
         nombre: "",
         asunto: "",
@@ -16,7 +43,7 @@ function Form() {
     })
 
     function handleChange(e) {
-        setForm({ ...form, [e.target.name]: e.target.value})
+        setForm({ ...form, [e.target.name]: e.target.value })
     }
 
     return (
@@ -29,15 +56,15 @@ function Form() {
                 </div>
                 <div className="flex flex-row gap-4">
                     <label className="text-white/70" htmlFor="">Asunto</label>
-                    <input className="w-1/2 bg-transparent border border-white/20 rounded-xl px-4 text-white" type="text " value={form.asunto} name="asunto"  placeholder="Presupuesto" onChange={handleChange} />
+                    <input className="w-1/2 bg-transparent border border-white/20 rounded-xl px-4 text-white" type="text " value={form.asunto} name="asunto" placeholder="Presupuesto" onChange={handleChange} />
                     <label className="text-white/70" htmlFor="">Telefono</label>
                     <input className="w-1/2 bg-transparent border border-white/20 rounded-xl px-4 text-white" type="text" value={form.telefono} name="telefono" placeholder="(+1)9935-4498" onChange={handleChange} />
                 </div>
-                <label className="text-white/70"  htmlFor="">Correo</label>
+                <label className="text-white/70" htmlFor="">Correo</label>
                 <input className="bg-transparent border border-white/20 rounded-xl px-4 text-white" type="text" value={form.correo} name="correo" placeholder="JhonDoe@mail.com" onChange={handleChange} />
-                <label className="text-white/70"  htmlFor="">Mensaje</label>
+                <label className="text-white/70" htmlFor="">Mensaje</label>
                 <textarea className="bg-transparent border border-white/20 rounded-xl px-4 py-3 text-white focus:outline-none focus:border-white/60 resize-none h-32" type="text" name="mensaje" value={form.mensaje} placeholder="Mensaje" onChange={handleChange} />
-                <div  className="flex items-center justify-center">
+                <div className="flex items-center justify-center">
                     <input className="rounded-xl cursor-pointer text-white bg-linear-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-linear-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:ring-blue-800 font-medium rounded-base px-4 py-2.5 text-center leading-5 text-xl" type="button" value="Enviar" />
                 </div>
             </form>
